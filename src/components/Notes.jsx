dc3d3f1f663bd87439610165ea9d06c357207830
Piyush Mohanty <98302219+Piyush-Mohanty02@users.noreply.github.com>
2023-04-09 21:05:56 +0530

    Initial commit

---
+++ src/components/Notes.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1>This is the React Bootcamp</h1>
+      <p>
+        I'm Piyush Mohanty from Odisha and I enjoy these bootcamps very much.
+      </p>
+    </div>
+  );
+}
+
+export default Notes;
